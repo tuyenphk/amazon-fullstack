73e92db734315ddae45dc7b0dca0e99c05872ef9
Tuyen Pham <tuh39709@temple.edu>
2021-03-22 15:31:14 -0400

    add header

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" className="header-logo" alt="" />
+            <div className="header-search">
+                <input className="header-searchInput" text="input" />
+                <SearchIcon className="header-searchIcon" />
+            </div>
+            <div className="header-nav">
+                <div className="header-option">
+                    <span className="header-optionLineOn">Hello Guest</span>
+                    <span className="header-optionLineTwo">Sign In</span>
+                </div>
+                <div className="header-option">
+                    <span className="header-optionLineOn">Returns</span>
+                    <span className="header-optionLineTwo">& Orders</span>
+                </div>
+                <div className="header-option">
+                    <span className="header-optionLineOn">Your</span>
+                    <span className="header-optionLineTwo">Prime</span>
+                </div>
+                <div className="header-optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header-optionLineTwo header-basketCount">0</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0AC2a47naMYgyDvawZe6q91761WU6yAI",
+  authDomain: "clone-9a4d5.firebaseapp.com",
+  projectId: "clone-9a4d5",
+  storageBucket: "clone-9a4d5.appspot.com",
+  messagingSenderId: "931963552528",
+  appId: "1:931963552528:web:37a4f87534d1715ac2db95",
+  measurementId: "G-VYMPF5QB9N",
+};
