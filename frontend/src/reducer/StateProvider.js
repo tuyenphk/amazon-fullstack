498fca267cb7a2e756578115c0d499fd4bc33482
Tuyen Pham <tuh39709@temple.edu>
2021-03-22 21:56:49 -0400

    fix error in StateProvider

--- frontend/src/reducer/StateProvider.js
+++ frontend/src/reducer/StateProvider.js
@@ -1,12 +1,12 @@
 import React, {createContext, useContext, useReducer} from 'react'
 
 //prepare dataLayer
-export const StateContext = createContext;
+export const StateContext = createContext();
 
 //wrap our app and provide the Data Layer
 export const StateProvider = ({reducer, initialState, children}) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
-        children
+        {children}
     </StateContext.Provider>
 )
 
