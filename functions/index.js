cbc8254f668b5a4036505ea52f751bc639f94730
Tuyen Pham <tuh39709@temple.edu>
2021-03-24 16:49:20 -0400

    set up payment on backend

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,31 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51IYabQH0yrwdQqcxFtu6Pq8IQHZRjPm7YELTPbFURF39lGNdPZxL8wPj8UDeRSUl5c8k9mRTq3LIlyub4QxTv3tG00MLEW5Yb9');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// App config
+const app = express();
+
+// Middle-wares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => response.status(200).send('hello'));
+app.post('/payments/create', async(request,response) => {
+    const total = request.query.total;
+    console.log('Payment Request is: ', total);
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+    //OK, created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret;
+    }) 
+})
+
+// Listen command
+exports.api = functions.https.onRequest(app);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create ({
-    baseURL: ''
+    baseURL: 'http://localhost:5001/clone-9a4d5/us-central1/api' //the API cloud function URL
 })
 
 export default instance;
