838ae825bfdc21f97a8d2d0db31642489e849995
Tuyen Pham <tuh39709@temple.edu>
2021-03-24 22:12:55 -0400

    thank you page

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Orders.css'
+import {db} from '../../firebase'
+import {useStateValue} from '../../reducer/StateProvider'
+import Order from '../../components/Order/Order'
+import CheckoutProduct from '../../components/CheckoutProduct/CheckoutProduct';
 
 function Orders() {
+    const [orders, setOrders] = useState([]);
+    const [{basket, user}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        if(user){
+            db.collection('users')
+              .doc(user?.uid)
+              .collection('orders')
+              .orderBy('created','desc')
+              .onSnapshot(snapshot => (
+              setOrders(snapshot.docs.map(doc => ({
+                  id: doc.id,
+                  data: doc.data()
+              })))
+          ))
+        } else {
+            setOrders([])
+        }       
+    }, [user])
+
     return (
         <div className="order">
-            <h1>Your orders are:</h1>
+            <h1>Thank you for your Order</h1>
+            <h2>We will send the confirmation email via your email with the tracking number when shipping.</h2>
+            <div className="orders-order">
+                {orders?.map(order => {
+                    <Order order={order} />
+                })}
+                {basket.map(item => (
+                    <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
